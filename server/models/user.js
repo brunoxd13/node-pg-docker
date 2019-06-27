5f549d4a6df2f518d9eb5c591754a7ecb1f40cd8
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2019-06-27 09:38:07 -0300

    fix: ajusted db name

--- server/models/user.js
+++ server/models/user.js
@@ -1,15 +1,11 @@
-"use strict";
+'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    "User",
-    {
-      name: DataTypes.STRING,
-      username: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING
-    },
-    {}
-  );
+  const User = sequelize.define('User', {
+    name: DataTypes.STRING,
+    username: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {});
   User.associate = function(models) {
     // associations can be defined here
   };
