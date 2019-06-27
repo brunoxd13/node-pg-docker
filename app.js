f405f2f6a9384b186a0b45d94901fc958bea61a9
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2019-06-27 09:37:44 -0300

    fix: run migration on start app

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./server/routes");
-const db = require("./models");
 
 const hostname = "0.0.0.0";
 const port = 3000;
@@ -14,11 +13,10 @@ routes(app);
 
 app.get("*", (req, res) =>
   res.status(200).send({
-    message: "Welcome to the ."
+    message: "Welcome to the API."
   })
 );
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-  db.sequelize.sync();
 });
